5a0f39673517c8795654b86ca2833950fb37edce
danielcastelluccio <danielrcastelluccio@gmail.com>
2025-10-14 20:33:32 -0600

    use short enums

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 xxd -i src/internal.lang > src/internal_source.h
 
-gcc src/main.c src/lexer.c src/ast.c src/parser.c src/processor.c src/evaluator.c src/common.c src/value.c src/llvm_codegen.c src/util.c src/string_view.c src/stb/ds.c -lLLVM -o lang -g -Wall -Wextra -Werror
+gcc src/main.c src/lexer.c src/ast.c src/parser.c src/processor.c src/evaluator.c src/common.c src/value.c src/llvm_codegen.c src/util.c src/string_view.c src/stb/ds.c -lLLVM -o lang -g -Wall -Wextra -Werror -fshort-enums
